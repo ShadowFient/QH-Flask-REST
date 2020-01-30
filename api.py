df4351ca92942760112c92ddfd2841251108851f
Roy Huang <huang.2928@osu.edu>
2020-01-30 00:13:09 -0500

    simple modification

--- api.py
+++ api.py
@@ -2,28 +2,25 @@ from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
 import mysql.connector as sql
+import credentials
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
-with open("dbcredential") as db_file:
-    ip = db_file.readline().strip()
-    password = db_file.readline()
-
-db = sql.connect(host=ip,
-                 user="hyyyy",
-                 passwd=password,
-                 database="quantum")
-
 
 class HelloWorld(Resource):
 
     def get(self):
-        cursor = db.cursor()
+        conn = sql.connect(host=credentials.HOST,
+                           user=credentials.USER,
+                           passwd=credentials.PASSWD,
+                           database="quantum")
+        cursor = conn.cursor()
         stmt = "select msg from test where msg_id=1;"
         cursor.execute(stmt)
         result = cursor.fetchone()
+        conn.close()
         return {'hello': result}
 
 
