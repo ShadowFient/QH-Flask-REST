8545d1b416c4876ca30600a33c5834352a82b81b
Roy Huang <huang.2928@osu.edu>
2020-02-06 14:15:26 -0500

    added api of workload for each pod

--- api.py
+++ api.py
@@ -1,30 +1,13 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_cors import CORS
-import mysql.connector as sql
-import credentials
+from workload import Workload
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
-
-class HelloWorld(Resource):
-
-    def get(self):
-        conn = sql.connect(host=credentials.HOST,
-                           user=credentials.USER,
-                           passwd=credentials.PASSWD,
-                           database="quantum")
-        cursor = conn.cursor()
-        stmt = "select msg from test where msg_id=1;"
-        cursor.execute(stmt)
-        result = cursor.fetchone()
-        conn.close()
-        return {'hello': result}
-
-
-api.add_resource(HelloWorld, '/')
+api.add_resource(Workload, '/workload')
 
 if __name__ == '__main__':
     app.run(debug=True)
