d611983c219426e9d4ec502f3436b6e321570360
Roy Huang <huang.2928@osu.edu>
2020-02-08 22:46:46 -0500

    manually merged with 'activity'

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from workload import Workload
+from activity import Activity
 import fetch_data
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ cors = CORS(app)
 
 api.add_resource(fetch_data.FetchData, '/fetch_data')
 api.add_resource(Workload, '/workload')
+api.add_resource(Activity, "/activity")
 
 if __name__ == '__main__':
     app.run(debug=True)
