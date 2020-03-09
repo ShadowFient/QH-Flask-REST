62860137e190d7360a575c4764a449f0b0dac8a5
meowattack <cai.476@osu.edu>
2020-03-09 12:38:25 -0400

    added pod no. to client psr

--- client_psr.py
+++ client_psr.py
@@ -8,7 +8,7 @@ class client_psr(Resource):
     def format(self,data):
         formatted_data = {}
         for idx, val in enumerate(data):
-            formatted_data[val[0]] = val[1]
+            formatted_data[val[0]] = [val[1],val[2]]
         return formatted_data
 
     def get(self):
@@ -20,7 +20,7 @@ class client_psr(Resource):
                            database="quantum")
         cursor = conn.cursor()
         #NEEDS TO BE CHANGED
-        stmt = "SELECT Group_Name,sum(PERC_TOTAL_PSR_PHONE) AS total_psr FROM quantum.model_output_data GROUP BY Group_Name"
+        stmt = "SELECT m.Group_Name, m.PERC_TOTAL_PSR_PHONE, p.INITIAL_POD FROM (SELECT Group_Name, sum(PERC_TOTAL_PSR_PHONE) PERC_TOTAL_PSR_PHONE FROM quantum.model_output_data GROUP BY Group_Name) m INNER JOIN quantum.pods_clients_map p WHERE m.Group_Name = p.Group_Name;"
         cursor.execute(stmt)
         result = cursor.fetchall()
         conn.close()
