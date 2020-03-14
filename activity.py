03ebe902b30d40e3308faae6add49a45b024c621
Roy Huang <huang.2928@osu.edu>
2020-03-14 10:20:04 -0400

    merged with `fix/activity`

--- activity.py
+++ activity.py
@@ -1,58 +1,56 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import mysql.connector as sql
 import credentials
+import pandas as pd
 
 
 class Activity(Resource):
 
     @staticmethod
     def get():
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", default="Initial Config")
+        config_name = parser.parse_args()["name"].replace("\'", "\"")
+
         conn = sql.connect(host=credentials.HOST,
                            user=credentials.USER,
                            passwd=credentials.PASSWD,
                            database="quantum")
-        cursor = conn.cursor()
-        stmt = ("select GroupID, Group_Name, Month, PCGPDC_TIME_HOURS_SUCC,"
-                " PCGPDC_TIME_HOURS_UNSUCC, PCGPAC_TIME_HOURS_SUCC,"
-                " PCGPAC_TIME_HOURS_UNSUCC, PCGFLLUP_TIME_HOURS_SUCC,"
-                " PCGFLLUP_TIME_HOURS_UNSUCC, PCGNEWALERT_TIME_HOURS_SUCC,"
-                " PCGNEWALERT_TIME_HOURS_UNSUCC, PCGREF_TIME_HOURS_SUCC,"
-                " PCGREF_TIME_HOURS_UNSUCC, PCGTERM_TIME_HOURS_SUCC,"
-                " PCGTERM_TIME_HOURS_UNSUCC, PCGEMPGRP_TIME_HOURS_SUCC,"
-                " PCGEMPGRP_TIME_HOURS_UNSUCC from model_output_data ;")
-        stmt1 = "select INITIAL_POD, GroupID from pods_clients_map;"
 
-        # get POD and Group ID
-        cursor.execute(stmt1)
-        pod = cursor.fetchall()
-        group = {}
-        num0 = cursor.rowcount
-        # match POD and Group ID
-        for i in range(0, num0):
-            group[pod[i][1]] = pod[i][0]
+        pods_query = "select POD from pods"
+        activity_query = "select `INITIAL_POD`, output.`GroupID`," \
+                         " output.Group_Name, Month, " \
+                         " PCGPDC_TIME_HOURS_SUCC," \
+                         " PCGPDC_TIME_HOURS_UNSUCC, " \
+                         " PCGPAC_TIME_HOURS_SUCC," \
+                         " PCGPAC_TIME_HOURS_UNSUCC, " \
+                         " PCGFLLUP_TIME_HOURS_SUCC," \
+                         " PCGFLLUP_TIME_HOURS_UNSUCC, " \
+                         " PCGNEWALERT_TIME_HOURS_SUCC," \
+                         " PCGNEWALERT_TIME_HOURS_UNSUCC, " \
+                         " PCGREF_TIME_HOURS_SUCC," \
+                         " PCGREF_TIME_HOURS_UNSUCC, " \
+                         " PCGTERM_TIME_HOURS_SUCC," \
+                         " PCGTERM_TIME_HOURS_UNSUCC, " \
+                         " PCGEMPGRP_TIME_HOURS_SUCC," \
+                         " PCGEMPGRP_TIME_HOURS_UNSUCC" \
+                         " from `pods_clients_map` map" \
+                         " inner join `model_output_data` output" \
+                         " on map.`GroupID` = output.`GroupID`" \
+                         " where map.`INITIAL_POD` = {}" \
+                         " and map.`Config_Name` = '{}';"
 
-        # get data
-        cursor.execute(stmt)
-        data = cursor.fetchall()
-        num = cursor.rowcount
-        result_list = []
-        cmp = ["POD","GroupID", "Group_Name", "Month", "PCGPDC_TIME_HOURS_SUCC",
-               "PCGPDC_TIME_HOURS_UNSUCC", "PCGPAC_TIME_HOURS_SUCC",
-               "PCGPAC_TIME_HOURS_UNSUCC", "PCGFLLUP_TIME_HOURS_SUCC",
-               "PCGFLLUP_TIME_HOURS_UNSUCC",
-               "PCGNEWALERT_TIME_HOURS_SUCC", "PCGNEWALERT_TIME_HOURS_UNSUCC",
-               "PCGREF_TIME_HOURS_SUCC",
-               "PCGREF_TIME_HOURS_UNSUCC", "PCGTERM_TIME_HOURS_SUCC",
-               "PCGTERM_TIME_HOURS_UNSUCC",
-               "PCGEMPGRP_TIME_HOURS_SUCC", "PCGEMPGRP_TIME_HOURS_UNSUCC"]
-        for i in range(0, num):
-            result = {}
-            for j in range(0, len(cmp)):
-                if j == 0:
-                    result[cmp[j]] = group[data[i][j]]
-                    j = j+1
-                result[cmp[j]] = data[i][j-1]
-            result_list.append(result)
+        pods_df: pd.DataFrame = pd.read_sql(pods_query, conn)
+        pods_df = pods_df.drop_duplicates()
+
+        activity_df: pd.DataFrame = pd.DataFrame()
+
+        for row in pods_df.itertuples():
+            each_df = pd.read_sql(activity_query.format(row.POD, config_name),
+                                  conn)
+            each_df["INITIAL_POD"] = row.POD
+            activity_df = activity_df.append(each_df, ignore_index=True)
+
         conn.close()
 
-        return result_list
+        return activity_df.to_dict('records')
